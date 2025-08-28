9e73c3d30e54bb4a10707b7a408f9d26c726e6a6
Youtski1 <zena03355@gmail.com>
2025-08-28 11:56:19 +0300

    update env

--- src/kbs.py
+++ src/kbs.py
@@ -1,7 +1,11 @@
 from aiogram.types import InlineKeyboardButton as ibut
 from aiogram.utils.keyboard import InlineKeyboardBuilder as builder
+from os import environ
+
+
+LINK_WEB_APP = environ.get("LINK_WEB_URL")
 
 def kb_start_webapp():
     return builder([[
-        ibut(text="😈 бить 😈", url="http://192.168.100.8:3000")
+        ibut(text="😈 бить 😈", url=LINK_WEB_APP)
     ]]).as_markup()
